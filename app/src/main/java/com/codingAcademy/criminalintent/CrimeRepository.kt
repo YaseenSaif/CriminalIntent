7409df6919e556a0d20cd53d2b815d08bbeb327d
Yaseen Al-Shaibani <yasenshibane77@gmail.com>
2020-10-18 09:10:06 +0300

    init commit

---
+++ app/src/main/java/com/codingAcademy/criminalintent/CrimeRepository.kt
@@ -0,0 +1,34 @@
+package com.codingAcademy.criminalintent
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.Room
+import database.CrimeDatabase
+import java.util.*
+
+private const val DATABASE_NAME = "crime-database"
+class CrimeRepository private constructor(context: Context) {
+   // fun getCrimes(): List<Crime> = crimeDao.getCrimes()
+    fun getCrimes(): LiveData<List<Crime>> = crimeDao.getCrimes()
+   // fun getCrime(id: UUID): Crime? = crimeDao.getCrime(id)
+    fun getCrime(id: UUID): LiveData<Crime?> = crimeDao.getCrime(id)
+    private val database : CrimeDatabase = Room.databaseBuilder(
+        context.applicationContext,
+        CrimeDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+    private val crimeDao = database.crimeDao()
+
+    companion object {
+        private var INSTANCE: CrimeRepository? = null
+        fun initialize(context: Context) {
+            if (INSTANCE == null) {
+                INSTANCE = CrimeRepository(context)
+            }
+        }
+        fun get(): CrimeRepository {
+            return INSTANCE ?:
+            throw IllegalStateException("CrimeRepository must be initialized")
+        }
+    }
+}
